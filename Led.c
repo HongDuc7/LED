81374f037a57dd7d03aab65bfd6a5f54d7ff8edd
HongDuc7 <vohongduc610@gmail.com>
2025-09-09 00:15:58 +0700

    Images

--- Led.c
+++ Led.c
@@ -578,7 +578,7 @@ static void MX_GPIO_Init(void)
     __HAL_RCC_GPIOD_CLK_ENABLE();
     __HAL_RCC_GPIOA_CLK_ENABLE();
 
-    // Cấu hình PA0 (nút bấm)
+    // Cấu hình PA0 (nút bấm thay đổi)
     GPIO_InitStruct.Pin = GPIO_PIN_0;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
     GPIO_InitStruct.Pull = GPIO_PULLDOWN;
